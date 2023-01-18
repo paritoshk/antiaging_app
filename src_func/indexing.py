bd3864a1744614eb41e5a30fb173e26f135c0681
Paritosh Kulkarni <pck2123@columbia.edu>
2023-01-18 18:59:51 -0500

    Update indexing.py
    cleaned it up

--- src_func/indexing.py
+++ src_func/indexing.py
@@ -10,20 +10,21 @@ import numpy as np
 import pickle
 
 
-#df = df.fillna('') # remove NaNs
-# Use pandas to read files from S3 buckets!
+# Use pandas to read files and clean
 df = pd.read_csv('data/publications/final_database_of_papers.csv', index_col=0)
-"""" function that sepearates empty rows in a specific column in a dataframe and returns two dataframes one wiht non-empty rows and other with empty rows
-input is a column and a dataframe
-outputs are two dataframes"""
 
 def remove_empty_rows(df, column):
+    """" function that sepearates empty rows in a specific column in a dataframe
+    and returns two dataframes one wiht non-empty rows and other with empty rows
+    input is a column and a dataframe
+    outputs are two dataframes"""
+
     df_empty = df[df[column].isna()]
     df_nonempty = df[df[column].notna()]
     return df_empty, df_nonempty
 
 df_noindex, df_report_nonempty_abs = remove_empty_rows(df, 'abstract')
-
+# I tried
 #https://huggingface.co/sentence-transformers/allenai-specter
 #model = SentenceTransformer('sentence-transformers/allenai-specter')
 
@@ -42,7 +43,7 @@ index = faiss.IndexFlatL2(embeddings.shape[1])
 
 # Step 3: Pass the index to IndexIDMap
 index = faiss.IndexIDMap(index)
-df.columns
+# check the columns - df.columns
 
 
 # Step 4: Add vectors and their IDs
@@ -50,51 +51,19 @@ index.add_with_ids(embeddings, df_report_nonempty_abs.article_id.values)
  # type: ignore
 D, I = index.search(np.array([embeddings[541]]), k=10)
  # type: ignore
- 
+
 #test it 
 print(f'L2 distance: {D.flatten().tolist()}\n\n PubMed paper IDs: {I.flatten().tolist()}')
 df.iloc[541, :]
 df[df['article_id'] == 31455183]
 
-#pickle the index for first 1000 abstracts 
+#pickle the index for abstracts 
 with open("/Users/paritoshmacmini/Documents/antiagingintegratedinformationsystem/antiaging/pickle_files/fiass_index.pickle", "wb") as h:
     pickle.dump(faiss.serialize_index(index), h)
     
     
-#pickle the embeddings for first 1000 abstracts 
+#pickle the embeddings for abstracts 
 with open("/Users/paritoshmacmini/Documents/antiagingintegratedinformationsystem/antiaging/pickle_files/embeddings.pickle", "wb") as h:
     pickle.dump(embeddings, h)
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-
-    
 #test pickle objects \\
-data = pd.read_csv('data/publications/post2018_agingcomapnies_papers.csv', index_col=0)   
-filter_company= ['brainstorm cell therapeutics','altos labs']
-frame = data[data['company_name'].isin(filter_company)]
-frame
-frame.columns
-frame.doi[0]
-
-
-
-
-
-
-
-# from transformers import AutoTokenizer, RoBERTaMultiNER2
-
-#tokenizer = AutoTokenizer.from_pretrained("dmis-lab/bern2-ner")
-
-#model = RoBERTaMultiNER2.from_pretrained("dmis-lab/bern2-ner")
