77469189b9cbb2fe44c2576b12bd6a14ba776dde
paritoshk <pck2123@columbia.edu>
2023-01-24 16:00:18 -0800

    authours

--- app.py
+++ app.py
@@ -143,9 +143,7 @@ def highlight_company_auths(match_string:str,case_df:pd.DataFrame)->list:
                     match_index.append(i)
             else:
                 continue
-    df_copy = case_df.copy(deep=True)
-    df_copy.iloc[match_index] = df_copy.iloc[match_index].style.apply(lambda x: ["background: lightpink"])
-    return df_copy
+    return match_index
 
 @st.cache(allow_output_mutation=True)
 def load_bert_model():
@@ -313,11 +311,12 @@ def main():
                                 st.markdown('<p class="keyword-font">{0}</p>'.format(i), unsafe_allow_html=True)
                 else:
                         st.markdown('<p class="keyword-font">{0}</p>'.format(f.iloc[0].keywords), unsafe_allow_html=True)
-                
                 # highlight author whose affliation is in the company list
-                with st.expander("Show information about authors and affiliations"):
-                        st.table(highlight_company_auths(f.iloc[0].company_name,author_frame))
-                
+                with st.expander("Show  affiliations working in the company"):
+                            index_temp = highlight_company_auths(f.iloc[0].company_name,author_frame)
+                            frame_sample = (author_frame.iloc[index_temp]).reset_index(drop=True)
+                            st.table(frame_sample.style.apply(lambda x: ["background: lighpink"]))
+
 
 
         else:
@@ -356,8 +355,11 @@ def main():
                                 st.markdown('<p class="keyword-font">{0}</p>'.format(i), unsafe_allow_html=True)
                     else:
                         st.markdown('<p class="keyword-font">{0}</p>'.format(f.iloc[0].keywords), unsafe_allow_html=True)
-                    with st.expander("Show information about authors and affiliations"):
-                        st.table(highlight_company_auths(f.iloc[0].company_name,author_frame))
+                    with st.expander("Show  affiliations working in the company"):
+                            index_temp = highlight_company_auths(f.iloc[0].company_name,author_frame)
+                            frame_sample = (author_frame.iloc[index_temp]).reset_index(drop=True)
+                            st.table(frame_sample.style.apply(lambda x: ["background: skyblue"]))
+
 
             
             except:
